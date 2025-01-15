883592bb967f4a5f1c997b515f98405d50a087a9
mungsehari <mungse.hari@gmail.com>
2025-01-15 18:56:32 +0530

    add email generatoe service

---
+++ src/main/java/com/hari/service/EmailGeneratorService.java
@@ -0,0 +1,68 @@
+package com.hari.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hari.request.EmailRequest;
+
+@Service
+public class EmailGeneratorService {
+    private final WebClient webClient;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public String generateEmailReply(EmailRequest emailRequest) {
+        String prompt = buildPrompt(emailRequest);
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[] {
+                        Map.of("parts", new Object[] {
+                                Map.of("text", prompt)
+                        })
+                }
+
+        );
+        String respones = webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve().bodyToMono(String.class)
+                .block();
+
+        return extractResponseContent(respones);
+
+    }
+
+    private String extractResponseContent(String respones) {
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(respones);
+            return rootNode.path("candidates").get(0).path("content").path("parts").get(0).path("text").asText();
+        } catch (Exception e) {
+            return "Error processing request " + e.getMessage();
+        }
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append(
+                "Generate a professional email reply for hte following email contents. Please don't generate a subject line ");
+        if (emailRequest.getTone() != null && emailRequest.getTone().isEmpty()) {
+            prompt.append("Use a").append(emailRequest.getTone()).append(" tone");
+        }
+        prompt.append("\n Origin email : \n ").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
+}
